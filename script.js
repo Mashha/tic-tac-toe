51bf5e94aa68504fba0376246df2e16881ff56d8
Masa Orel <orel.masha89@gmail.com>
2022-11-02 09:47:05 +0100

    fixed computer choice to have a has in array

--- script.js
+++ script.js
@@ -1,78 +1,70 @@
 //game board array - module
 const gameBoard = (function () {
-  const board = ["null","null","null","null","null","null","null","null","null"]
+  const board = [null, null, null, null, null, null, null, null, null]
 
-  
   return {
     board,
   }
 })()
 
-
 // // controls the view
-const displayController = (function(){
-    let container = document.querySelector(".board-boxes")
-    
-     function renderBoard(gameBoard) {
-      clearTheBoard()
-          gameBoard.forEach(function(item, index) {
-            let box = document.createElement("div")
-            box.classList.add("box")
-            if(item === "null"){
-              box.textContent = ""
-              box.id = index
-            } else {
-              box.textContent = item
-            }
-              container.appendChild(box)
-          });
-     }
+const displayController = (function () {
+  let container = document.querySelector('.board-boxes')
 
-     function clearTheBoard() {
-      container.textContent = ""
-     }
+  function renderBoard(gameBoard) {
+    clearTheBoard()
+    gameBoard.forEach(function (item, index) {
+      let box = document.createElement('div')
+      box.classList.add('box')
+      if (item === null) {
+        box.textContent = ''
+        box.id = index
+      } else {
+        box.textContent = item
+      }
+      container.appendChild(box)
+    })
+  }
 
+  function clearTheBoard() {
+    container.textContent = ''
+  }
 
-     return{
-      container,
-      renderBoard
-     }
+  return {
+    container,
+    renderBoard,
+  }
 })()
 
 displayController.renderBoard(gameBoard.board)
 
-
 // game flow
-const userController = (function(){
-  let boardContainer = document.querySelector(".board-boxes")
+const userController = (function () {
+  let boardContainer = document.querySelector('.board-boxes')
 
-  boardContainer.addEventListener("click",function(e){
-    if(e.target.textContent === ""){
-      gameBoard.board[e.target.id] = "X"
-      computerChoice()
-      displayController.renderBoard(gameBoard.board) 
-     
+  boardContainer.addEventListener('click', function (e) {
+    if (e.target.textContent === '') {
+      gameBoard.board[e.target.id] = 'X'
+      displayController.renderBoard(gameBoard.board)
+      setTimeout(() => {
+        computerChoice()
+        displayController.renderBoard(gameBoard.board)
+      }, 1000)
     } else {
       return
     }
-
   })
-  function computerChoice(){
-    let computer = Math.floor(Math.random() * 9)
-    if(gameBoard.board[computer] === "null") {
-      gameBoard.board[computer] = "O"
-    } else {
-      let newBoard = gameBoard.board.filter(item => item === "null")
-      let newComputerChoice = Math.floor(Math.random() * newBoard.length)
-       gameBoard.board[newComputerChoice] = "O"
-        }
-        
-    
+
+  function computerChoice() {
+    let emptySpaces = []
+
+    for (let i = 0; i < gameBoard.board.length; i++) {
+      if (gameBoard.board[i] === null) {
+        emptySpaces.push(i)
+      }
     }
-     
 
-  
-
+    let computer = emptySpaces[Math.floor(Math.random() * emptySpaces.length)]
+    gameBoard.board[computer] = 'o'
+  }
 })()
-
-
